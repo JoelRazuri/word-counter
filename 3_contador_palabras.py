c241da69285cee1ff09b02e39ecd30cb7a231eae
JoelRazuri <106347517+JoelRazuri@users.noreply.github.com>
2022-10-12 19:54:54 -0300

    Add files via upload

---
+++ 3_contador_palabras.py
@@ -0,0 +1,23 @@
+"""
+    Preguntamos al usuario en que está pensando. Cuando se introduce la respuesta, realizará el conteo de palabras en la sentencia e imprimimos en 
+    la salida el resultado.
+
+    Ejemplo:
+
+    Pregunta: ¿En qué estás pensando?
+    Entrada: Bien, hoy es el día en el que me voy a crear un desarrollador experto
+    Salida: ¡Muy bien, tu me has mostrado tu pensamiento en 15 palabras!
+"""
+
+import os 
+
+contador = 1
+frase = input("En que estas pensando?: ")
+
+for i in range(len(frase)):
+    if frase[i]==" ":
+        contador+=1
+
+print(f"Entrada: {frase}")
+print(f"Salida: Muy bien, tu me has mostrado tu pensamiento en {contador} palabras.")
+
